8bd60ce174399bb7b59ab3aca22f6683814fbf4f
Yannick <tech@yalit.be>
2025-01-07 12:03:40 +0100

    build : install/update  messenger

--- .castor/composer.php
+++ .castor/composer.php
@@ -18,3 +18,12 @@ function composer_require(#[AsRawTokens] array $arg, bool $dev = false): void
 
     Docker::exec($command);
 }
+
+#[AsTask(name:"composer:install", description: "Install the composer modules")]
+function composer_install(): void
+{
+    io()->text("Installing all composers dependencies");
+    $command = ['composer', 'install'];
+
+    Docker::exec($command);
+}
