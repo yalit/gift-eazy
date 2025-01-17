e420e6c7457645f5ab786eff3089b426110065a4
Yannick <tech@yalit.be>
2025-01-17 23:24:42 +0100

    feat : update Event with token

--- app/src/Entity/Event/Event.php
+++ app/src/Entity/Event/Event.php
@@ -7,8 +7,14 @@ use App\Repository\EventRepository;
 use DateTimeImmutable;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\GreaterThanOrEqual;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\NotNull;
 
 #[ORM\Entity(repositoryClass: EventRepository::class)]
+#[UniqueEntity('token')]
 class Event
 {
     #[ORM\Id]
@@ -17,13 +23,24 @@ class Event
     private ?int $id = null;
 
     #[ORM\Column(type: Types::STRING, length: 255)]
-    private string $name = '';
+    #[NotBlank]
+    #[NotNull]
+    private string $token;
+
+    #[ORM\Column(type: Types::STRING, length: 255)]
+    #[NotNull]
+    #[NotBlank]
+    private string $name;
 
     #[ORM\Column(type: Types::DATE_IMMUTABLE)]
+    #[GreaterThanOrEqual('now')]
     private DateTimeImmutable $date;
 
     #[ORM\Column(type: Types::STRING, length: 255)]
-    private string $organizerEmail = '';
+    #[NotBlank]
+    #[NotNull]
+    #[Email]
+    private string $organizerEmail;
 
     #[ORM\Column(type: Types::STRING, length: 255, nullable: true)]
     private ?string $organizerName = null;
@@ -31,11 +48,11 @@ class Event
     #[ORM\Column(type: Types::STRING, length: 255, enumType: EventStatus::class)]
     private EventStatus $status = EventStatus::DRAFT;
 
-    #[ORM\Column(type: Types::STRING, length: 255)]
-    private string $theme = '';
+    #[ORM\Column(type: Types::STRING, length: 255, nullable: true)]
+    private ?string $theme = null;
 
-    #[ORM\Column(type: Types::TEXT)]
-    private string $description = '';
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $description = null;
 
     #[ORM\Column(type: Types::INTEGER)]
     private int $maximumAmount = 0;
@@ -50,7 +67,7 @@ class Event
         return $this->id;
     }
 
-    public function getName(): ?string
+    public function getName(): string
     {
         return $this->name;
     }
@@ -110,24 +127,28 @@ class Event
         return $this;
     }
 
-    public function getTheme(): string
+    public function getTheme(): ?string
     {
         return $this->theme;
     }
 
-    public function setTheme(string $theme): void
+    public function setTheme(?string $theme): static
     {
         $this->theme = $theme;
+
+        return $this;
     }
 
-    public function getDescription(): string
+    public function getDescription(): ?string
     {
         return $this->description;
     }
 
-    public function setDescription(string $description): void
+    public function setDescription(?string $description): static
     {
         $this->description = $description;
+
+        return $this;
     }
 
     public function getMaximumAmount(): int
@@ -135,8 +156,22 @@ class Event
         return $this->maximumAmount;
     }
 
-    public function setMaximumAmount(int $maximumAmount): void
+    public function setMaximumAmount(int $maximumAmount): static
     {
         $this->maximumAmount = $maximumAmount;
+
+        return $this;
+    }
+
+    public function getToken(): string
+    {
+        return $this->token;
+    }
+
+    public function setToken(string $token): static
+    {
+        $this->token = $token;
+
+        return $this;
     }
 }
