93df7619caa9f195f00a5caeac130ba5f1b1426a
Yannick <tech@yalit.be>
2025-01-18 14:32:13 +0100

    test : add tests for Event Creation Process

--- app/tests/Integration/Process/Entity/EventCreationProcessTest.php
+++ app/tests/Integration/Process/Entity/EventCreationProcessTest.php
@@ -12,9 +12,12 @@ use App\Repository\EventRepository;
 use App\Repository\Security\PasswordResetTokenRepository;
 use App\Repository\Security\UserRepository;
 use DateTimeImmutable;
+use Exception;
 use Faker\Factory;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Messenger\Exception\ValidationFailedException;
+use Symfony\Component\Messenger\MessageBusInterface;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Uid\Uuid;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
@@ -26,37 +29,35 @@ use function PHPUnit\Framework\assertTrue;
 class EventCreationProcessTest extends KernelTestCase
 {
     private EventRepository $eventRepository;
-    private ValidatorInterface $validator;
-    private TranslatorInterface $translator;
+    private MessageBusInterface $messageBus;
 
     protected function setUp(): void
     {
         $this->eventRepository = self::getContainer()->get(EventRepository::class);
-        $this->validator = self::getContainer()->get(ValidatorInterface::class);
-        $this->translator = self::getContainer()->get(TranslatorInterface::class);
+        $this->messageBus = self::getContainer()->get(MessageBusInterface::class);
     }
 
     protected function tearDown(): void
     {
         parent::tearDown();
         unset($this->eventRepository);
-        unset($this->validator);
-        unset($this->translator);
+        unset($this->messageBus);
     }
 
     public function testCorrectEventCreation(): void
     {
         $faker = Factory::create();
-        $eventCreation = new EventCreation();
-        $eventCreation->name = $faker->sentence(1);
-        $eventCreation->organizerName = $faker->name;
-        $eventCreation->organizerEmail = $faker->email;
-        $eventCreation->description = $faker->text;
-        $eventCreation->date = DateTimeImmutable::createFromMutable($faker->dateTimeInInterval('now', '+10 months'));
-        $eventCreation->maximumAmount = 100;
+        $eventCreation = $this->getEventCreation(
+            $faker->sentence(1),
+            $faker->email(),
+            $faker->name(),
+            $faker->word(),
+            $faker->text,
+            DateTimeImmutable::createFromMutable($faker->dateTimeInInterval('now', '+10 months')),
+            $faker->numberBetween(10, 250)
+        );
 
-        $process = new EventCreationProcess($this->eventRepository);
-        $process($eventCreation);
+        $this->messageBus->dispatch($eventCreation);
 
         $createdEvent = $this->eventRepository->findOneBy(['organizerEmail' => $eventCreation->organizerEmail, 'name' => $eventCreation->name]);
         self::assertNotNull($createdEvent);
@@ -71,8 +72,119 @@ class EventCreationProcessTest extends KernelTestCase
         self::assertEquals($eventCreation->maximumAmount, $createdEvent->getMaximumAmount());
     }
 
-    public function testIncorrectEventCreation(): void
+    /**
+     * @dataProvider getIncorrectDTO
+     */
+    public function testInvalidEventCreation(EventCreation $eventCreation): void
     {
-        //TODO :: create the incorrect test
+        $this->expectException(ValidationFailedException::class);
+        $this->messageBus->dispatch($eventCreation);
+    }
+
+    /**
+     * @return iterable<string, array<int, EventCreation>>
+     */
+    public function getIncorrectDTO(): iterable
+    {
+        $faker = Factory::create();
+        yield "No Name" => [
+            $this->getEventCreation(
+                null,
+                $faker->email(),
+                $faker->name(),
+                $faker->word(),
+                $faker->text,
+                DateTimeImmutable::createFromMutable($faker->dateTimeInInterval('now', '+10 months')),
+                $faker->numberBetween(10, 250)
+            )
+        ];
+        yield "Blank Name" => [
+            $this->getEventCreation(
+                "",
+                $faker->email(),
+                $faker->name(),
+                $faker->word(),
+                $faker->text,
+                DateTimeImmutable::createFromMutable($faker->dateTimeInInterval('now', '+10 months')),
+                $faker->numberBetween(10, 250)
+            )
+        ];
+        yield "No Organizer email" => [
+            $this->getEventCreation(
+                $faker->sentence(1),
+                null,
+                $faker->name(),
+                $faker->word(),
+                $faker->text,
+                DateTimeImmutable::createFromMutable($faker->dateTimeInInterval('now', '+10 months')),
+                $faker->numberBetween(10, 250)
+            )
+        ];
+        yield "Blank Organizer email" => [
+            $this->getEventCreation(
+                $faker->sentence(1),
+                "",
+                $faker->name(),
+                $faker->word(),
+                $faker->text,
+                DateTimeImmutable::createFromMutable($faker->dateTimeInInterval('now', '+10 months')),
+                $faker->numberBetween(10, 250)
+            )
+        ];
+        yield "Organizer incorrect email" => [
+            $this->getEventCreation(
+                $faker->sentence(1),
+                $faker->name(),
+                $faker->name(),
+                $faker->word(),
+                $faker->text,
+                DateTimeImmutable::createFromMutable($faker->dateTimeInInterval('now', '+10 months')),
+                $faker->numberBetween(10, 250)
+            )
+        ];
+        yield "Date in the past" => [
+            $this->getEventCreation(
+                $faker->sentence(1),
+                $faker->email(),
+                $faker->name(),
+                $faker->word(),
+                $faker->text,
+                DateTimeImmutable::createFromMutable($faker->dateTimeBetween('-1 month', '-1 day')),
+                $faker->numberBetween(10, 250)
+            )
+        ];
+        yield "Negative maximum amount" => [
+            $this->getEventCreation(
+                $faker->sentence(1),
+                $faker->email(),
+                $faker->name(),
+                $faker->word(),
+                $faker->text,
+                DateTimeImmutable::createFromMutable($faker->dateTimeInInterval('now', '+10 months')),
+                $faker->numberBetween(-110, -1)
+            )
+        ];
+    }
+
+    public function getEventCreation(
+        ?string $name = null,
+        ?string $organizerEmail = null,
+        ?string $organizerName = null,
+        ?string $theme = null,
+        ?string $description = null,
+        DateTimeImmutable $date = new DateTimeImmutable(),
+        int $maximumAmount = 0,
+    ): EventCreation {
+        $eventCreation = new EventCreation();
+
+        $eventCreation->name = $name;
+        $eventCreation->organizerEmail = $organizerEmail;
+        $eventCreation->organizerName = $organizerName;
+        $eventCreation->theme = $theme;
+        $eventCreation->description = $description;
+        $eventCreation->date = $date;
+        $eventCreation->maximumAmount = $maximumAmount;
+
+        return $eventCreation;
     }
 }
